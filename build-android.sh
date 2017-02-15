aa85299ab6aa083818823fb6973170c6fc501ee9
Karsten Pawlik <rescan@kafenio.org>
2017-02-15 19:37:47 +0100

    version update, recompile with cordova 6.5.0, added zipalign to buildprocess

--- build-android.sh
+++ build-android.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+rm -f rescan.apk rescan_zalign.apk
+
 cordova-icon
 
 cordova build android --release
 
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk alias_name
 mv platforms/android/build/outputs/apk/android-release-unsigned.apk rescan.apk
+/Users/pawlikk/adt-bundle-mac-x86_64-20140702/sdk/build-tools/23.0.2/zipalign -f -v 4 rescan.apk rescan_zalign.apk
