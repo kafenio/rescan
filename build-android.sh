9e3410e2bf1915623729aa12a1c5507c8fab284d
Karsten Pawlik <karsten.pawlik@o2.com>
2019-10-22 16:09:04 +0200

    updated cordova and migrated config.xml to package.json

--- build-android.sh
+++ build-android.sh
@@ -8,4 +8,4 @@ cordova build android --release
 
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk alias_name
 mv platforms/android/build/outputs/apk/android-release-unsigned.apk rescan.apk
-/Users/pawlikk/adt-bundle-mac-x86_64-20140702/sdk/build-tools/23.0.2/zipalign -f -v 4 rescan.apk rescan_zalign.apk
+/Users/pawlikk/Library/Android/sdk/build-tools/29.0.2/zipalign -f -v 4 rescan.apk rescan_zalign.apk
